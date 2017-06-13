9acbb23a6a7824123b4ee2cd720afa1ad1474e9a
zhy <zhyack@126.com>
2017-06-13 17:08:48 +0800

    auto_push

--- data_utils/test.py
+++ data_utils/test.py
@@ -1,6 +1,6 @@
 # import data_prepare
 # data_prepare.getAllBook(0, 10000)
-# data_prepare.getAllBook(10001, 20000)
+data_prepare.getAllBook(10001, 20000)
 # data_prepare.getAllBook(20001, 30000)
 # data_prepare.getAllBook(30001, 40000)
 # data_prepare.getAllBook(40001, 50000)
@@ -8,11 +8,12 @@
 # data_prepare.getAllBook(0, 54866)
 
 
-import data_statistics
-data_statistics.init(rebuild=True, banlist=[[],['Various', 'Anonymous'],[]])
-# data_statistics.init(banlist=[[],['Various', 'Anonymous'],[]])
-from data_statistics import rootp
-data_statistics.queryCnt(p=rootp)
-data_statistics.queryCnt(p=rootp+'/00')
-data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='files')
-data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='words')
+# import data_statistics
+# data_statistics.init(rebuild=True, banlist=[[],['Various', 'Anonymous'],[]])
+# # data_statistics.init(banlist=[[],['Various', 'Anonymous'],[]])
+# from data_statistics import rootp
+# data_statistics.queryCnt(p=rootp)
+# data_statistics.queryCnt(p=rootp+'/00')
+# data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='files')
+# data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='words')
+# data_statistics.queryMax(p=rootp+'/00', depth=2, tp='file', metric='words')
