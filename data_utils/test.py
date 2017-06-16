70e688a0057b6eed0893eaf10fa63298a79a5243
zhy <zhyack@126.com>
2017-06-16 11:37:40 +0800

    auto_push

--- data_utils/test.py
+++ data_utils/test.py
@@ -1,33 +1,24 @@
-import data_prepare
-
-while True:
-    # ret = data_prepare.getAllBook(0, 10000)
-    # ret = data_prepare.getAllBook(10001, 20000)
-    ret = data_prepare.getAllBook(20001, 30000)
-    # ret = data_prepare.getAllBook(30001, 40000)
-    # ret = data_prepare.getAllBook(40001, 50000)
-    # ret = data_prepare.getAllBook(50001, 54866)
-    # ret = data_prepare.getAllBook(0, 54866)
-    if len(ret)==0:
-        break
+# import data_prepare
+#
+# while True:
+#     # ret = data_prepare.getAllBook(0, 10000)
+#     # ret = data_prepare.getAllBook(10001, 20000)
+#     ret = data_prepare.getAllBook(20001, 30000)
+#     # ret = data_prepare.getAllBook(30001, 40000)
+#     # ret = data_prepare.getAllBook(40001, 50000)
+#     # ret = data_prepare.getAllBook(50001, 54866)
+#     # ret = data_prepare.getAllBook(0, 54866)
+#     if len(ret)==0:
+#         break
 
 
 #
-# import data_statistics
-# # data_statistics.init(rebuild=True, banlist=[[],['Various', 'Anonymous'],[]])
+import data_statistics
+data_statistics.init(rebuild=True, banlist=[[],['Various', 'Anonymous'],[]])
 # data_statistics.init(banlist=[[],['Various', 'Anonymous'],[]])
-# from data_statistics import rootp
-# data_statistics.queryCnt(p=rootp)
-# data_statistics.queryCnt(p=rootp+'/00')
-# data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='files')
-# data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='words')
-# data_statistics.queryMax(p=rootp+'/00', depth=2, tp='file', metric='words')
-
-# base_data_dir='../data'
-# from base_ops import *
-# dlang = map2load(base_data_dir+'/lang.map')
-# dauthor = map2load(base_data_dir+'/author.map')
-# dbook = map2load(base_data_dir+'/book.map')
-# save2json(dlang, base_data_dir+'/lang.json')
-# save2json(dauthor, base_data_dir+'/author.json')
-# save2json(dbook, base_data_dir+'/book.json')
+from data_statistics import rootp
+data_statistics.queryCnt(p=rootp)
+data_statistics.queryCnt(p=rootp+'/00')
+data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='files')
+data_statistics.queryMax(p=rootp+'/00', depth=1, tp='folder', metric='words')
+data_statistics.queryMax(p=rootp+'/00', depth=2, tp='file', metric='words')
